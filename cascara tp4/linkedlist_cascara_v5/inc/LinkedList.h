f536a3295b630cbfe8803edb2a0e0fe6875e7e31
MelanyZap <48812742+MelanyZap@users.noreply.github.com>
2019-06-10 22:05:12 -0300

    Add files via upload

---
+++ cascara tp4/linkedlist_cascara_v5/inc/LinkedList.h
@@ -0,0 +1,55 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2018>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __LINKEDLIST
+#define __LINKEDLIST
+struct Node
+{
+    void* pElement; //direccion e memoria de comienzo del bloque del primer empleado
+    struct Node* pNextNode; //estructura autoreferenciada(porque hay un puntero en la misma estructura). Un puntero al proximo nodo.
+}typedef Node;
+
+struct LinkedList //en memoria creo el linkedlist
+{
+    Node* pFirstNode; //Va a apuntar a la direccion de memoria del primer elemento (Persona)
+    int size; //tamaño de la linkedlist
+}typedef LinkedList;
+#endif
+
+
+
+//Publicas
+LinkedList* ll_newLinkedList(void);
+int ll_len(LinkedList* this);
+Node* test_getNode(LinkedList* this, int nodeIndex);
+int test_addNode(LinkedList* this, int nodeIndex,void* pElement);
+int ll_add(LinkedList* this, void* pElement);
+void* ll_get(LinkedList* this, int index);
+int ll_set(LinkedList* this, int index,void* pElement);
+int ll_remove(LinkedList* this,int index);
+int ll_clear(LinkedList* this);
+int ll_deleteLinkedList(LinkedList* this);
+int ll_indexOf(LinkedList* this, void* pElement);
+int ll_isEmpty(LinkedList* this);
+int ll_push(LinkedList* this, int index, void* pElement);
+void* ll_pop(LinkedList* this,int index);
+int ll_contains(LinkedList* this, void* pElement);
+int ll_containsAll(LinkedList* this,LinkedList* this2);
+LinkedList* ll_subList(LinkedList* this,int from,int to);
+LinkedList* ll_clone(LinkedList* this);
+int ll_sort(LinkedList* this, int (*pFunc)(void* ,void*), int order);
